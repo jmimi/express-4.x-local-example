3b686172100ad8124d76cc3d0e825b486d8e38c4
Ebrahim Pasbani <e.pasbani@gmail.com>
2016-12-19 23:45:59 +0330

    show how strategy name : SO : 41229507/check-if-user-logged-in-by-passport-strategy-different-user-types

--- server.js
+++ server.js
@@ -11,11 +11,13 @@ var db = require('./db');
 // that the password is correct and then invoke `cb` with a user object, which
 // will be set at `req.user` in route handlers after authentication.
 passport.use(new Strategy(
-  function(username, password, cb) {
+{passReqToCallback: true},
+  function(req, username, password, cb) {
     db.users.findByUsername(username, function(err, user) {
       if (err) { return cb(err); }
       if (!user) { return cb(null, false); }
       if (user.password != password) { return cb(null, false); }
+	req.usedStrategy = 'custom';
       return cb(null, user);
     });
   }));
@@ -75,6 +77,7 @@ app.get('/login',
 app.post('/login', 
   passport.authenticate('local', { failureRedirect: '/login' }),
   function(req, res) {
+    console.log('Used Strategy is : ' + req.usedStrategy);
     res.redirect('/');
   });
   
